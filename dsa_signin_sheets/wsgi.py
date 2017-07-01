0f28d5b37d6454fcee5795a7e91bd637bac7ff7a
Jeremy Low <jeremylow@gmail.com>
2017-07-01 13:31:41 -0400

    demo

---
+++ dsa_signin_sheets/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dsa_signin_sheets project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dsa_signin_sheets.settings")
+
+application = get_wsgi_application()
