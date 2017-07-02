d45ccc8ed83442a30129a678c3509c67176d175e
Jeremy Low <jeremylow@gmail.com>
2017-07-02 19:00:58 -0400

    demo14

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'signin_sheets',
 ]
 
