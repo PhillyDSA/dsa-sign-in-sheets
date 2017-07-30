29459f6bcfabb9c7ca13c4fc5af703c0619074a0
Jeremy Low <jeremylow@gmail.com>
2017-07-30 13:01:44 -0400

    turn off debug mode

--- config/settings.py
+++ config/settings.py
@@ -29,8 +29,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'pre-deploy')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 
