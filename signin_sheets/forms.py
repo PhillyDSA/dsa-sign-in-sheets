c8c43cc7de95113713bb7b8b113a9e09410835ce
Jeremy Low <jeremylow@gmail.com>
2017-07-30 12:58:45 -0400

    turn off autocomplete for participant signin form

--- signin_sheets/forms.py
+++ signin_sheets/forms.py
@@ -11,6 +11,14 @@ from signin_sheets.models import EventParticipant
 class ParticipantSigninForm(ModelForm):
     """Form to sign in an event participant."""
 
+    def __init__(self, *args, **kwargs):
+        import ipdb; ipdb.set_trace()
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'autocomplete': 'off'
+            })
+
     class Meta:
         """Class specific attrs. Required for ModelForm."""
 
